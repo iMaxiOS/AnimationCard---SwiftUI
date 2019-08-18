399a3248b21f1fa8c8540fe74113baff14af7d0e
iMakca <ukr-maksim1@yandex.ru>
2019-08-18 15:06:50 +0300

    push project

---
+++ Animation Cards - SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Animation Cards - SwiftUI
+//
+//  Created by Maxim Granchenko on 18.08.2019.
+//  Copyright © 2019 Maxim Granchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
